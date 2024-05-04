b99e13c1292c0c939c20268ed86020fc82767e44
paugarcia32 <paugarcia32@gmail.com>
2024-05-04 18:34:03 +0200

    good practices config.h done v2

--- HackUPC-ESP32/HackUPC-ESP32.ino
+++ HackUPC-ESP32/HackUPC-ESP32.ino
@@ -1,15 +1,7 @@
-#include <WiFi.h>
+
+#include "config.h" // Incluye el archivo config.h que acabamos de crear
 #include <HTTPClient.h>
-// #include <DebouncedLDR.h>
-
-// const char* ssid = "HACKUPC2024B";
-// const char* password = "Biene2024!";
-const char* ssid = "pau";
-const char* password = "pau12345";
-const int LDR_PIN = 34;
-// DebouncedLDR ldr(1023, 9, 100);
-
-int init1;
+#include <WiFi.h>
 
 void setup() {
   Serial.begin(115200);
@@ -17,39 +9,22 @@ void setup() {
 
   Serial.println("Conectando a Wi-Fi");
   WiFi.begin(ssid, password);
-  // init1 = 1;
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Conectando...");
   }
-
 }
 
 void loop() {
-  // if (init1){
-
-  //   init1 = 0;
-  // }
-  
-
   Serial.println("Conexión establecida");
   int sensorValue = analogRead(LDR_PIN);
   Serial.println(sensorValue);
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
 
-    String serverAddress = "http://192.168.72.18:5000/data/create";
     http.begin(serverAddress);
-    
-    // Leer el valor del sensor
-    // unsigned int sensorValue = analogRead(LDR_PIN);
-    // int sensorValue = 33;
-    // Serial.println(sensorValue);
-    // Convertir el valor del sensor a una cadena
-    // String sensorValueStr = String(sensorValue);
 
-    // Construir el JSON con el valor del sensor
     String jsonPayload = "{";
     jsonPayload += "\"value\": ";
     jsonPayload += sensorValue;
@@ -60,7 +35,7 @@ void loop() {
 
     // Realizar la solicitud POST con el JSON
     int httpResponseCode = http.POST(jsonPayload);
-    
+
     if (httpResponseCode > 0) {
       String response = http.getString();
       Serial.println("Solicitud exitosa");
---
+++ HackUPC-ESP32/config.h
@@ -0,0 +1,15 @@
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Configuración de WiFi
+const char *ssid = "HACKUPC2024B";
+const char *password = "Biene2024!";
+
+// Configuración de IP del servidor
+const char *serverAddress = "http://10.192.230.83:5000/data/create";
+
+// Pin del sensor LDR
+const int LDR_PIN = 34;
+
+#endif
