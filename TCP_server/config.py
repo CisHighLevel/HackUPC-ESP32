a6fb0b5987c300f09d633d02f0b20a47763012f5
paugarcia32 <paugarcia32@gmail.com>
2024-05-04 12:29:51 +0200

    good practices config.py done

---
+++ TCP_server/config.py
@@ -0,0 +1,4 @@
+
+ServerIP = "192.168.72.52"
+ServerPort = 1999
+bufferSize = 1024
--- TCP_server/tcp_Server.py
+++ TCP_server/tcp_Server.py
@@ -1,8 +1,6 @@
-import socket
 
-ServerIP = "10.192.230.83"
-ServerPort = 1999
-bufferSize = 1024
+import socket
+from config import ServerIP, ServerPort, bufferSize
 
 TCPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -19,35 +17,29 @@ print("TCP server connected to server and ready to perform read write operations
 print(f"Connected by client {address}")
 
 receivedData = ""
-bufferSize = 1024
 
 print("Server is waiting for incoming data")
 
-switchState = 1
-
 while True:
-    
-        try:
-            receivedData = clientSocket.recv(bufferSize).decode('utf-8')
-            if not receivedData:
-                break
-            else:
-                print("[FromClient]: " + receivedData)
-                if "close" in receivedData:
-                    clientSocket.close()
-                    print("Client has disconnected")
-                    print("Close socket server")
-                    TCPServerSocket.close()
-                    break
-        except KeyboardInterrupt:
-            print("\nKeyboard interrupt detected. Continuing execution.")
-            continue
-        except ConnectionResetError:
-            print("Client closed connection unexpectedly")
-            clientSocket.close()
-            TCPServerSocket.close()
+    try:
+        receivedData = clientSocket.recv(bufferSize).decode('utf-8')
+        if not receivedData:
             break
+        else:
+            print("[FromClient]: " + receivedData)
+            if "close" in receivedData:
+                clientSocket.close()
+                print("Client has disconnected")
+                print("Close socket server")
+                TCPServerSocket.close()
+                break
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt detected. Continuing execution.")
+        continue
+    except ConnectionResetError:
+        print("Client closed connection unexpectedly")
+        clientSocket.close()
+        TCPServerSocket.close()
+        break
 
-        print("Server is waiting to receive data from the keyboard")
-        # switchState = not switchState
-    
+    print("Server is waiting to receive data from the keyboard")
